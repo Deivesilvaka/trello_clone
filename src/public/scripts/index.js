04076c427f95ab4c2be5b4658145f0602e1b98f2
Luiz Davi <luizdaviinformatica@gmail.com>
2021-06-30 10:37:49 -0300

    ajustes de largura

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,9 +1,12 @@
 
 import Board from "./createBoard.js"
+let tamanho = 0
 
 async function start() {
     const response = await fetch('/getDatas')
     const board = await response.json()
+
+    tamanho = board.db.length
 
     await Board.createBoard(board.db)
 }
@@ -11,6 +14,10 @@ async function start() {
 await start()
 
 setInterval(async() => {
+
+    console.log(tamanho)
+
+    document.querySelector("#container").style.width = tamanho < 5 ? `${5 * 250}px` : `${tamanho * 250}px`
     
     if(!localStorage.getItem("updated")){
         localStorage.setItem("updated", "YES")
